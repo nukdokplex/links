ba4760b61860e86252813ddceb4b98ce60dada0c
NukDokPlex <nukdokplex@nukdotcom.ru>
2023-08-21 16:24:59 +0500

    fixed footer margins

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import './Footer.css';
 function Footer(props) {
     const { copyrightText } = props;
     return (
-        <div className='footer py-2 mt-2 text-center'>
-            <p dangerouslySetInnerHTML={{__html: copyrightText}}></p>
+        <div className='footer py-4 mt-2 text-center'>
+            <p className='mb-0' dangerouslySetInnerHTML={{__html: copyrightText}}></p>
         </div>
     );
 }
